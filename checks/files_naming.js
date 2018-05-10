57bbd9946e00290861dc7118eda689689dc02065
Zachary Williams <wil11063@byui.edu>
2018-05-10 08:48:07 -0600

    Statistics report & fix for old names

--- checks/files_naming.js
+++ checks/files_naming.js
@@ -1,7 +1,7 @@
 const canvas = require('canvas-api-wrapper');
 const fileType = require('../misc_scripts/fileType.js');
 
-module.exports = (item, logger) => {
+module.exports = (item, logger, course) => {
 
     function logFile() {
         logger.log(`${item.constructor.name} | Does Not Meet Naming Conventions`, {
@@ -13,8 +13,8 @@ module.exports = (item, logger) => {
     if (item.constructor.name !== 'File') {
         return;
     } else if ((item.display_name.match(/_/g) && item.display_name.match(/_/g).length) !== 2 ||
-        fileType(item.display_name) !== item.display_name.split('_')[1]) {
-        // ALSO NEED TO MATCH courseCode
+        fileType(item.display_name) !== item.display_name.split('_')[1] ||
+        item.display_name.split('_')[0] !== course.courseDetails.course_code.replace(/\s/gi, '').toLowerCase()) {
         logFile();
     }
 
