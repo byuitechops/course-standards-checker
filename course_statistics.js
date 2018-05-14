7a6f09fe8c0970fdce3736a5122fa45b80c225ac
Zachary Williams <wil11063@byui.edu>
2018-05-14 13:31:09 -0600

    Updates and improvements across the baord

--- course_statistics.js
+++ course_statistics.js
@@ -1,27 +1,27 @@
 module.exports = (logger, course) => {
 
     /* Category Item Results */
-    logger.log('Course Stastics', {
+    logger.log('Course Statistics', {
         'Category': 'Number of Modules',
         'Result': course.modules.length
     });
-    logger.log('Course Stastics', {
+    logger.log('Course Statistics', {
         'Category': 'Number of Files',
         'Result': course.files.length
     });
-    logger.log('Course Stastics', {
+    logger.log('Course Statistics', {
         'Category': 'Number of Discussions',
         'Result': course.discussions.length
     });
-    logger.log('Course Stastics', {
+    logger.log('Course Statistics', {
         'Category': 'Number of Assignments',
         'Result': course.assignments.length
     });
-    logger.log('Course Stastics', {
+    logger.log('Course Statistics', {
         'Category': 'Number of Quizzes',
         'Result': course.quizzes.length
     });
-    logger.log('Course Stastics', {
+    logger.log('Course Statistics', {
         'Category': 'Number of Pages',
         'Result': course.pages.length
     });
@@ -47,9 +47,14 @@ module.exports = (logger, course) => {
     }
 
     /* Total Course File Size */
-    logger.log('Course Stastics', {
+    logger.log('Course Statistics', {
         'Category': 'Combined File Size',
         'Result': `${course.files.reduce((acc, file) => acc + file.size, 0)/1000000} MB`
     });
 
+    // TODO number of qualtrics
+    // TODO number of equella links
+    // TODO harvard business school links
+    // TODO round combined file size
+    // TODO add object, audio, video tags
 };
